e0a96eefc4ebe7587810b771663319b73836b202
Daniel Joyce <danielalexanderjoyce@gmail.com>
2022-09-16 12:40:10 +0100

    index cahnges

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,12 @@
+require('dotenv').config()
 import express from "express";
 const bodyParser = require("body-parser");
 const app = express();
-import recommendRouter from "./routes/recommend"
-import ratingRouter from "./routes/rating"
 import gameRouter from "./routes/game"
 
 app.use(bodyParser.json());
 
 // Routes
-app.use("/recommend", recommendRouter);
-app.use("/rating", ratingRouter);
 app.use("/game", gameRouter);
 
 app.listen(2460, () => {
