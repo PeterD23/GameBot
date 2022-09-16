dc4d7f657f9c198e9e388b1f66d4ed8ebe698a2c
Daniel Joyce <danielalexanderjoyce@gmail.com>
2022-09-16 12:41:44 +0100

    removed dist folder.

--- server/dist/routes/game.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-describe('Game routes', () => {
-    it('should give back a positive response when both a HLTB and gameRating are given', () => {
-    });
-});
